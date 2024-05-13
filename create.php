818021c90844daa6e187bb75f7bd7191f4841ad7
Moein Mirsaeedi <Moein.Mirsaeedi@gmail.com>
2024-05-13 22:38:03 +0330

    Create "panel" and "create post" page

---
+++ create.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?= asset("css/styles.css") ?>" />
+    <link rel="stylesheet" href="<?= asset("css/panel.css") ?>" />
+</head>
+<body>
+    <main>
+            <nav>
+                <ul>
+                    <li><a href="#">Panel</a></li>
+                    <li><a href="#">Create post</a></li>
+                </ul>
+            </nav>
+            <section class="content">
+                <div>
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title">
+                </div>
+                <div>
+                    <label for="category">Category</label>
+                    <select name="category" id="category">
+                        <option value="political">Political</option>
+                        <option value="sport">Sport</option>
+                        <option value="social">Social</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="content">Content</label>
+                    <textarea name="contenbt" id="content" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label for="image">Image</label>
+                    <input type="file" name="image" id="image">
+                </div>
+                <div>
+                    <input type="submit" value="Login">
+                </div>
+            </section>
+        </main>
+</body>
+</html>
--- panel.php
+++ panel.php
@@ -14,8 +14,110 @@ if(!authenticated()){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel</title>
+    <link rel="stylesheet" href="<?= asset("css/styles.css") ?>" />
+    <link rel="stylesheet" href="<?= asset("css/panel.css") ?>" />
 </head>
 <body>
-    <a href="logout.php">Logout</a>
+    <main>
+            <nav>
+                <ul>
+                    <li><a href="#">Panel</a></li>
+                    <li><a href="#">Create post</a></li>
+                </ul>
+            </nav>
+            <section class="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>View</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>this is a post</td>
+                            <td>politiacl</td>
+                            <td>150 view</td>
+                            <td>2021 May 05</td>
+                            <td>
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </main>
 </body>
 </html>
